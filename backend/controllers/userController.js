ed5c0ec415c34aa0ffebf95acdab8327272b69f1
wizgurl101 <nb.phan@hotmail.com>
2021-08-01 12:47:48 -0600

    implement authorizating log in user in backend.  Successsful tested in Postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,4 +45,35 @@ const registerNewUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerNewUser };
+/**
+ * @desc Authentication of login user
+ * @route POST /api/users/login
+ * @access Public
+ */
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  // check input email was valid
+  if (!user) {
+    res.status(401);
+    throw new Error("Invalid email.");
+  }
+
+  const isPasswordValid = await user.matchPassword(password);
+  // check input password was valid
+  if (!isPasswordValid) {
+    res.status(401);
+    throw new Error("Invalid password");
+  }
+
+  // if user exists and input password match with database, send back respond with user info
+  res.json({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+    token: generateJWT(user._id),
+  });
+});
+
+export { registerNewUser, authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { registerNewUser } from "../controllers/userController.js";
+import { registerNewUser, authUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/").post(registerNewUser);
+router.post("/login", authUser);
 
 export default router;
